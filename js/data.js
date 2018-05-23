32456061b490cc165bc13570098ad30f558077ad
sayalisd <36068215+sayalisd@users.noreply.github.com>
2018-05-23 11:19:11 +0530

    Add files via upload
    updated knockout details

--- js/data.js
+++ js/data.js
@@ -1,129 +1,198 @@
-var places = [
+var placesData = [
 {
-  "name": "Golkonda Fort",
-  "location": {"lat" : 17.383309, "lng" : 78.4010528},
+  "name": "Golkonda",
+  "location": {"lat" : 17.383309, "lng" : 78.4010528}
 },
 {
   "name": "Charminar",
-  "location": {"lat" : 17.3615636, "lng" : 78.4746645},
+  "location": {"lat" : 17.3615636, "lng" : 78.4746645}
 },
 {
   "name": "Salarjung Museum",
-  "location": {"lat" : 17.3713224, "lng" : 78.4803589},
+  "location": {"lat" : 17.3713224, "lng" : 78.4803589}
 },
 {
   "name": "Chowmahalla Palace",
-  "location": {"lat" : 17.3578233, "lng" : 78.4716897},
+  "location": {"lat" : 17.3578233, "lng" : 78.4716897}
 },
 {
   "name": "Ramoji Film City",
-  "location": {"lat" : 17.254301, "lng" : 78.680767},
+  "location": {"lat" : 17.254301, "lng" : 78.680767}
 }
-]
+];
 
-// Create global variables
 var map,
-	infowindow,
-	bounds;
-var $wikiElem = $('#wikipedia-links');
+	bounds,
+	wikiurl;
+	
+// initialize the map
+function initMap(){
 
-//initMap() is called when page is loaded
-function initMap() {
-  map = new google.maps.Map(document.getElementById("map"),{center: {lat: 17.385044, lng: 78.486671},zoom: 13,mapTypeControl: false});
-  infowindow = new google.maps.InfoWindow();
-  bounds = new google.maps.LatLngBounds();
+    map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 13,
+        center: {lat: 17.385044, lng: 78.486671}
+    });
 
-  // Close infowindow when clicked elsewhere on the map
-  map.addListener("click", function(){
-    infowindow.close(infowindow);
-  });
-  
-  // Recenter map upon window resize
-  window.onresize = function () {
+    infoWindow = new google.maps.InfoWindow({
+    maxWidth: 150,
+    content: ""
+	});
+
+    bounds = new google.maps.LatLngBounds();
+
+    for(var i=0; i<placesData.length; i++){
+        addMarker(placesData[i]);
+        bounds.extend(placesData[i].location);
+    }
     map.fitBounds(bounds);
-  };
+}
 
-  //Creating Place object
-  var Place = function (data, id, map) {
-    var self = this;
-    this.name = ko.observable(data.name);
-    this.location = data.location;
-    this.marker = "";
-    this.markerId = id;
-  };
-  
-  function getContent(place) {
-	var wikiUrl = 'https://en.wikipedia.org/w/api.php?action=opensearch&search='+place.name+'&format=json&origin=*';	
-    var contentString = place.wikiUrl
-    var errorString = "Wikipedia not working"
-    if (place.name.length > 0) {
-      return contentString;
-      } else {
-      return errorString;
-      }
-  }
-  
- function ViewModel() {
-    var self = this;
 
-    // Nav button control
-    this.isNavClosed = ko.observable(false);
-    this.navClick = function () {this.isNavClosed(!this.isNavClosed());};
+// this function creates a new marker and adds it to the observableArray of markers
+function addMarker(place){
+    var coordinates = {
+        lat: place.location.lat,
+        lng: place.location.lng
+    };
 
-    // Creating list elements from the placeList
-    this.placeList = ko.observableArray();
-    places.forEach(function(item){self.placeList.push(new Place(item));});
-
-    // Create a marker per place item
-    this.placeList().forEach(function(place) {
-      var marker = new google.maps.Marker({
+    self.marker = new google.maps.Marker({
         map: map,
-        position: place.location,
-        animation: google.maps.Animation.DROP
-      });
-      place.marker = marker;	  
-	  bounds.extend(marker.position);
-	  
-      // Create an onclick event to open an infowindow and bounce the marker at each marker
-      marker.addListener("click", function(e) {
-        map.panTo(this.position);
-        map.panBy(0, -200);
-		infowindow.setContent(getContent(place));
-        infowindow.open(map, marker);
+        position: coordinates,
+        animation: google.maps.Animation.DROP,
+        clickable: true
+    });
+
+    if (self.marker){
+        self.markerArray().push([coordinates, self.marker]);
+        google.maps.event.addListener(self.marker, "click", function(){
+		showWikiInfo(place);
+        stopAnimation();
+        animateMarker(coordinates);
+		});
+    }
+}
+
+
+// This function hides all the markers from the map by setting their 'visible' option to false
+function hideMarkers(){
+  for(var i=0; i<self.markerArray().length; i++){
+    self.markerArray()[i][1].setVisible(false);
+  }
+}
+
+// This function shows all the markers on the map by setting their 'visible' option to true
+function displayMarkers(){
+  for(var i=0; i<self.markerArray().length; i++){
+    self.markerArray()[i][1].setVisible(true);
+  }
+}
+
+
+// This function drops the selected marker when the corresponding list item is clicked
+function animateMarker(coordinate){
+  ko.computed(function(){
+    ko.utils.arrayForEach(self.markerArray(), function(marker){
+      if(coordinate.lat === marker[0].lat && coordinate.lng === marker[0].lng){
+        marker[1].setVisible(true);
+        marker[1].setAnimation(google.maps.Animation.DROP);
+      }
     });
   });
-
-  
-    // Creating click for the list item
-    this.itemClick = function (place) {
-      var markerId = place.markerId;
-      google.maps.event.trigger(place.marker, "click");
-    }
-
-    // Filtering the Place list
-    self.filter = ko.observable("");
-
-    this.filteredPlaceList = ko.dependentObservable(function() {
-      var q = this.filter().toLowerCase();
-      if (!q) {
-      // Returns the original array;
-      return ko.utils.arrayFilter(self.placeList(), function(item) {
-        item.marker.setVisible(true);
-        return true;
-      });
-      } else {
-		  return ko.utils.arrayFilter(this.placeList(), function(item) {
-          if (q.toLowerCase().indexOf(q) >= 0) {
-          return true;
-          } else {
-            item.marker.setVisible(false);
-          return false;
-          }
-        });
-      }
-    }, this);
-  };
-
- // Activates knockout.js
-ko.applyBindings(new ViewModel());
 }
+
+// This function stops the animation of all the markers on the map
+function stopAnimation(){
+  for(var i=0; i<self.markerArray().length; i++){
+    self.markerArray()[i][1].setAnimation(null);
+  }
+}
+
+// shows the marker that corresponds with the given place
+function displayMarkersPlace(place){
+  for(var i=0;i<self.markerArray().length; i++){
+    if(place.location.lat == self.markerArray()[i][0].lat && place.location.lng == self.markerArray()[i][0].lng){
+      self.markerArray()[i][1].setVisible(true);
+    }
+  }
+}
+
+
+// this function uses the wikipedia api to get url to get info about place and show on sidebar
+function showWikiInfo(place){
+	var placename = place.name;
+	var urlString = "https://en.wikipedia.org/w/api.php?action=opensearch&search="+placename+"&format=json&origin=*";
+
+    $.ajax({
+        url: urlString,
+        dataType: "json",
+        async: true,
+        success: function(data){
+			wikiurl = data[3][0];
+			self.placeInfo(wikiurl);
+        },
+        error: function(data){
+            wikiError();
+        }
+    });
+}
+
+
+var ViewModel = function(){
+    var self = this;
+
+    this.markerArray = ko.observableArray([]);
+    this.searchQuery = ko.observable();
+
+    this.placeName = ko.observable();
+    this.placeInfo = ko.observable();
+
+    this.apiError = ko.observable(false);
+    this.errorMessage = ko.observable();
+
+    // filters locations displayed in the list based on the given search query
+    this.searchResult = ko.computed(function(){
+        query = self.searchQuery();
+        if (!query){
+            displayMarkers();
+            return placesData;
+        } else {
+            hideMarkers();
+            var filteredPlaces = [];
+            for (var i=0; i<placesData.length; i++){
+                if (placesData[i].name.toLowerCase().indexOf(query) >= 0){
+                    displayMarkersPlace(placesData[i]);
+                    filteredPlaces.push(placesData[i]);
+                }
+            }
+            return filteredPlaces;
+        }
+
+    });
+
+    // displays the wikipedia info for the given place and animates the corresponding marker on the map
+    this.viewPlaceOnMap = function(place){
+        var coordinate = {lat: place.location.lat, lng: place.location.lng};
+        stopAnimation();
+        hideMarkers();
+        animateMarker(coordinate);
+        showWikiInfo(place);
+    };
+
+};
+
+
+// This function is called when there is an error loading the Google Maps API
+function mapError(){
+  self.errorMessage("Error in accessing google maps");
+  self.apiError(true);
+}
+
+// This function is called when there is an error while loading data from the Wikipedia API
+function wikiError(){
+  self.errorMessage("Error in accessing wikipedia");
+  self.apiError(true);
+}
+
+
+// apply bindings
+ko.applyBindings(ViewModel());
